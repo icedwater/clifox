4643e1775f85a4ec1a6881e12b33738ea43f5ad4
Brandon McGinty-Carroll <bmmcginty@bmcginty.hopto.org>
2013-06-08 16:58:31 -0400

    add bookmarksTreeview with ability to activate particular bookmarks

--- guiTree.py
+++ guiTree.py
@@ -5,11 +5,11 @@ import os,os.path,sys,curses,time
 class treeview(object):
  space=" "*200
 #uncomment the below open statement and comment the below return statement to enable logging again
-# log=open("/tmp/tree","wb")
+ log=open("/tmp/tree","wb")
  def l(self,*a):
-  return
-#  [self.log.write(str(i)+"\n") for i in a]
-#  self.log.flush()
+#  return
+  [self.log.write(str(i)+"\n") for i in a]
+  self.log.flush()
 
  def __init__(self,nodes):
   self.screen=None
@@ -36,14 +36,15 @@ class treeview(object):
   self.show()
 #  self.screen.move(self.screen.getyx()[0]+1,0)
 
- def up(self):
+ def up(self,show=1):
   self.l("up:","curIndex:",self.curIndex)
   if self.curIndex<=0:
    return 1
   self.curIndex-=1
   self.cur=self.list[self.curIndex]
   self.level=self.levels[self.cur]
-  self.show()
+  if show==1: self.show()
+  return 0
 
  def markCollapsed(self,root):
   i=self.list.index(root)+1
@@ -52,21 +53,28 @@ class treeview(object):
    self.l("markCollapsed:","i:",i,"listI:",self.list[i],"level:",self.levels[self.list[i]])
    if self.levels.get(self.list[i],level)<level:
     break
-   self.collapseFunc(self.list[i])
+   if self.states[self.list[i]]==1: self.collapseFunc(self.list[i])
    self.states[self.list[i]]=0
    i+=1
   return i
 
- def collapse(self):
-  self.l("collapse:","cur:",self.cur,"canCollapse:",self.canCollapse(self.cur),"state:",self.states[self.cur])
-  if self.canCollapse(self.cur) and self.states.get(self.cur,1)==1:
-   newIndex=self.markCollapsed(self.cur)
-   rem=[self.list.pop(self.curIndex+1) for i in xrange(self.curIndex+1,newIndex)]
-   rem.insert(0,self.cur)
-   self.collapseFunc(rem)
-   self.states[self.cur]=0
-   self.l("collapsed:"+str(self.cur)+" list:"+str(self.list))
-   self.show()
+ def collapse(self,i=None):
+  i=self.cur if i==None else i
+  self.l("collapse:","cur:",i,"canCollapse:",self.canCollapse(i),"state:",self.states[i])
+  if self.canCollapse(i) and self.states.get(i,1)==1:
+   newIndex=self.markCollapsed(i)
+   startIndex=self.list.index(i)+1
+   rem=[self.list.pop(startIndex) for j in xrange(startIndex,newIndex)]
+   rem.insert(0,i)
+#   self.collapseFunc(rem)
+   self.states[i]=0
+   self.l("collapsed:"+str(i)+" list:"+str(self.list))
+  else:
+   while self.states[self.cur]==0:
+    ret=self.up(show=0)
+    if ret==1:
+     break
+  self.show()
 
  def expand(self):
   self.l("expand:","curIndex:",self.curIndex,"cur:",self.cur,"canExpand:",self.canExpand(self.cur),"state:",self.states[self.cur])
@@ -77,6 +85,12 @@ class treeview(object):
    [self.states.__setitem__(i,0) for i in new]
    [self.levels.__setitem__(i,self.level+1) for i in new]
    self.show()
+
+ def canReturn(self,i):
+  return 1
+
+ def getReturnable(self,i):
+  return str(i)
 
  def show(self):
   where=self.curIndex
@@ -108,9 +122,15 @@ class treeview(object):
   self.screen.touchwin()
   self.screen.refresh()
 
+ def collapseEntireList(self):
+  t=0
+  while sum(self.states.values())>0:
+   self.collapse([i for i in self.list[::-1] if self.states[i]==1][0])
+   t+=1
+  return t
+
  def __call__(self, screen):
   self.screen = screen
-  curses.def_shell_mode()
   self.screen.nodelay(1)
   self.screen.keypad(1)
   curses.raw(1)
@@ -129,7 +149,7 @@ class treeview(object):
 #each time a key is pressed, it should be shown here.
 #write spaces to top line of window for clear line
    if k==ord('q'):
-    sys.exit()
+    return None
    if k==curses.KEY_LEFT:
     self.collapse()
    if k==curses.KEY_RIGHT:
@@ -139,8 +159,9 @@ class treeview(object):
    if k==curses.KEY_DOWN:
     self.down()
    if k in (curses.KEY_ENTER,10):
-    pass #return self.cur
-#   self.show()
+    if self.canReturn(self.cur):
+     self.collapseEntireList()
+     return self.getReturnable(self.cur)
    continue
 
 class fileTreeview(treeview):
@@ -177,6 +198,58 @@ class nodeTreeview(treeview):
 
  def collapseFunc(self,x):
   pass
+
+class bookmarkTree(treeview):
+ def __init__(self,*a,**kw):
+  self.js=kw.pop('js')
+  treeview.__init__(self,*a,**kw)
+
+ def canExpand(self,x):
+  i=x[1]
+  return i.type in [i.RESULT_TYPE_DYNAMIC_CONTAINER,i.RESULT_TYPE_QUERY,i.RESULT_TYPE_FOLDER,i.RESULT_TYPE_FOLDER_SHORTCUT]
+
+ def canCollapse(self,i):
+  return self.canExpand(i)
+
+ def expandFunc(self,x):
+  i=x[1]
+  _i=i
+  i=self.getSpecificType(_i)
+  ret=[]
+  if i.containerOpen!=None:
+   i.containerOpen=1
+#  title,uri=i.title,i.uri
+   if i.hasChildren:
+    children=[i.getChild(j) for j in xrange(i.childCount)]
+    [ret.append((j.title if j.title!=None else '',j)) for j in children]
+  return ret
+
+ types=None
+ def getSpecificType(self,i):
+  if self.types==None:
+   self.types={
+i.RESULT_TYPE_URI:self.js.Ci.nsINavHistoryResultNode,
+i.RESULT_TYPE_VISIT:self.js.Ci.nsINavHistoryVisitResultNode,
+i.RESULT_TYPE_FULL_VISIT:self.js.Ci.nsINavHistoryFullVisitResultNode,
+i.RESULT_TYPE_DYNAMIC_CONTAINER:self.js.Ci.nsINavHistoryContainerResultNode,
+i.RESULT_TYPE_QUERY:self.js.Ci.nsINavHistoryQueryResultNode,
+i.RESULT_TYPE_FOLDER:self.js.Ci.nsINavHistoryQueryResultNode,
+i.RESULT_TYPE_SEPARATOR:self.js.Ci.nsINavHistoryResultNode,
+i.RESULT_TYPE_FOLDER_SHORTCUT:self.js.Ci.nsINavHistoryQueryResultNode
+}
+  try:
+   return i.QueryInterface(self.types[i.type])
+  except:
+   return i
+
+ def collapseFunc(self,i):
+  i[1].containerOpen=0
+
+ def canReturn(self,i):
+  return 1 if not self.canExpand(i) else 0
+
+ def getReturnable(self,i):
+  return str(i[1].uri)
 
 if __name__=='__main__':
  #text,obj
