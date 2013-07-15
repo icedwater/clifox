62a63aa84a981097195879286d47f52c7d6494ed
Brandon McGinty-Carroll <bmmcginty@bmcginty.hopto.org>
2013-07-15 14:41:12 -0400

    add configParser file to separate configuration from main file

---
+++ configParser.py
@@ -0,0 +1,32 @@
+import os,os.path,utils,curses
+config=utils.attrDict()
+
+def defaultConfig():
+ global config
+#replace with parser for name.x.y.z=value, resolvable with config.name.x.y.z
+#set dom to parse js
+ config.logging=1
+ config.homepage="http://www.bmcginty.hopto.org/form.html"
+ config.pagePosition="percentage"
+ config.caseSensitiveSearch="false"
+ config.showInstantPage=0
+ config.mainWindowKeys={
+ord("`"):"self.nextWindow()",
+ord("q"):"self.quit()",
+ord("\n"):"self.execute()",
+curses.KEY_DOWN:"self.nextLine()",
+curses.KEY_UP:"self.prevLine()",
+curses.KEY_RIGHT:"self.nextChar()",
+curses.KEY_LEFT:"self.prevChar()"
+}
+
+if os.path.exists('/etc/wb.conf'): execfile('/etc/wb.conf')
+if os.path.exists('./wb.conf'): execfile('./wb.conf')
+if os.path.exists(os.path.expanduser('~/.wb/wb.conf')):
+ execfile(os.path.expanduser('~/.wb/wb.conf'))
+if not os.path.exists(os.path.expanduser('~/.clifox/')):
+ os.mkdir(os.path.expanduser('~/.clifox'))
+if os.path.exists(os.path.expanduser('~/.clifox/clifox.conf')):
+ execfile(os.path.expanduser('~/.clifox/clifox.conf'))
+if not config.keys():
+ defaultConfig()
