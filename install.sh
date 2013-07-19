128610dd2a2041bc32b2c4d3c37f68b90e2a76f9
Brandon McGinty-Carroll <bmmcginty@bmcginty.hopto.org>
2013-07-19 02:27:39 -0400

    install.sh:add checks for xvfb failing, ability to download and use firefox nightly, and replacing cat/timeout with read call

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 root=$(pwd)
 arch=$(uname -m)
-echo "warning. this install will remove all saved firefox settings from your profile. This includes all thunderbird messages, firefox favorites, etc. You have ten seconds to cancel this install."
-sleep 10
+ffpath="./ff/firefox"
+read -t10 -p "warning. this install will remove all saved firefox settings from your profile. This includes all thunderbird messages, firefox favorites, etc. You have ten seconds to cancel this install.
+continue (c-c to abort):"
 if [ ! -f "./clifox" ]
 then
 echo "You are not in the root of your source tree. Please change to the top-level directory, containing the clifox binary and the firefox and mozrepl directorys."
@@ -12,7 +13,20 @@ if [ -d ~/.mozilla/firefox ]
 then
 rm -Rf ~/.mozilla/firefox
 fi
-if [ ! -f "./ff/firefox/firefox" ]
+if [[ $1 == "beta" ]]
+then
+mkdir -p ff/nightly
+cd ff/nightly
+if [ ! -f "firefox.nightly.tar.bz2" ]
+then
+path=$(wget -q -O- "http://nightly.mozilla.org/" | python -c 'import sys,re;print "\n".join(re.findall(r"\"(.*://.*bz2)\"",sys.stdin.read()))' | grep -i $(uname -m))
+wget -q -O "firefox.nightly.tar.bz2" "$path"
+fi
+bzcat firefox.nightly.tar.bz2 | tar -xf -
+ffpath="./ff/nightly/firefox"
+cd ../..
+else
+if [ ! -f "$ffpath/firefox" ]
 then
 mkdir ff
 cd ff
@@ -26,14 +40,25 @@ bunzip2 *.bz2
 tar -xf *.tar
 cd ..
 fi
-ff="./ff/firefox"
-if [ ! -f "$ff/firefox" ]
+fi
+if [ ! -f "$ffpath/firefox" ]
 then
-echo "firefox binary not found in $ff."
-exit 0
+echo "firefox binary not found in $ffpath.
+Perhaps your architecture, $arch, is not supported."
+exit 1
 fi
 echo "creating default profile by running firefox"
-timeout 10 xvfb-run "$ff/firefox"
+ret=$(timeout 10 xvfb-run "$ffpath/firefox")
+retcode=$?
+if [ $retcode != 124 ]
+then
+echo "An error occured. You might be already running a copy of xvfb, or you might have a stale X11 .lock file in your /tmp directory.
+If you want to be sure, kill all processes with xvfb in the name, and clear your /tmp directory.
+You can also run this installer with the environment variable NUM, set to a number lower than the current number.
+Displayed text was:
+$ret"
+exit 1
+fi
 echo "gathering profile paths"
 profid=$(ls -1 ~/.mozilla/firefox | grep -i default | head -n1)
 profdir=~/.mozilla/firefox/$profid
@@ -47,8 +72,17 @@ ln -s "`pwd`/mozrepl" "$profdir/extensions/mozrepl"
 echo "modifying extentions.ini"
 python -c "fh=open('$profdir/extensions.ini','rb');fc=fh.read();fh.close();p=fc.find('\n')+1;fc=fc[:p]+'Extension0=$profdir/extensions/mozrepl\n'+fc[p:];fh=open('$profdir/extensions.ini','wb');fh.write(fc);fh.flush();fh.close()"
 cp $profdir/extensions.ini $profdir/extensions2.ini
-timeout 5 xvfb-run $ff/firefox
-cp $profdir/extensions2.ini $profdir/extensions.ini 
+ret=$(timeout 5 xvfb-run $ffpath/firefox)
+retcode=$?
+if [ $retcode != 124 ]
+then
+echo "An error occured.
+Firefox could not be run after the modification to extensions.ini.
+Displayed text was:
+$ret"
+exit 1
+fi
+mv $profdir/extensions2.ini $profdir/extensions.ini 
 touch $profdir/extensions.ini
 echo "copying clifox config."
 if [ ! -d ~/.clifox ]
@@ -59,6 +93,7 @@ if [ ! -x ~/.clifox/clifox.conf ]
 then
 cp "$root/conf/clifox.conf" ~/.clifox/
 fi
-echo "running firefox. mozrepl should display a message to this console with its listening status."
-xvfb-run $ff/firefox
+echo "running firefox. mozrepl should display a message to this console with its listening status.
+You can leave firefox running in the background, or c-c this copy and run it in another terminal."
+xvfb-run $ffpath/firefox
 
